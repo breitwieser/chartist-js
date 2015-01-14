9e9761b919c7edbc3ca81967b574b43bfb777ca6
Georg Lausegger <georg.lausegger@student.tugraz.at>
2015-01-14 14:49:46 +0100

    added missing example file

---
+++ site/examples/example-parallel-coordinates.js
@@ -0,0 +1,13 @@
+var data = {
+  labels: ['foo', 'bar', 'test', 'bla', 'pc-var'],
+  series: [
+    [12, 9, 7, 8, 5],
+    [2, 1, 3.5, 7, 3],
+    [1, 3, 4, 5, 6]
+  ]
+};
+
+new Chartist.ParallelCoordinates('.ct-chart', data);
+
+/* Add a basic data series with six labels and values */
+
